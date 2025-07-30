fa054ece3615d7a2ecb8cef43689be687762aa0b
Rockruff <36600169+Rockruff@users.noreply.github.com>
2025-07-30 21:12:25 +0800

    chore: initialize frontend
    - Scaffolded the project using `npx create-next-app@15.4.5`
    - Removed boilerplate files and code from the default template
    - Added minimal placeholder components for future development
    
    Configuration used:
    - TypeScript: Yes
    - ESLint: No
    - Tailwind CSS: Yes
    - `src/` directory: Yes
    - App Router: Yes
    - Turbopack: Yes
    - Import alias customization: No

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen w-screen items-center justify-center bg-[var(--primary)]">
+      <img src="/logo-uwa.svg"></img>
+    </main>
+  );
+}
