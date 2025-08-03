ec6c04d4cfb00114a140e5dea420c560e28512bb
Rockruff <36600169+Rockruff@users.noreply.github.com>
2025-08-04 00:43:19 +0800

    feat(frontend): add <Transition> component for smooth UI animations
    - Install `clsx` for conditional className handling
    - Add a reusable <Transition> component for bidirectional transitions using Tailwind CSS classes
    - Update Prettier config to support formatting of multiline <Transition> props

--- frontend/prettier.config.mjs
+++ frontend/prettier.config.mjs
@@ -5,6 +5,10 @@ const frontendPrettierConfig = {
 
   // Add support for Tailwind CSS
   plugins: [...basePrettierConfig.plugins, "prettier-plugin-tailwindcss"],
+
+  // Config for prettier-plugin-tailwindcss
+  // Enable formatting for the parameters of the <Transition> component
+  tailwindAttributes: ["type", "before", "start", "end", "after"],
 };
 
 export default frontendPrettierConfig;
