6c9f76aa67517062f1fc5e3c5c5e983050111a71
Gayathri <gkkanakaratne@gmail.com>
2025-09-14 09:44:14 +0800

    Users serializers: detail/list/create/aliases + batch DTOs

--- backend/core/serializers/users.py
+++ backend/core/serializers/users.py
@@ -1,6 +1,7 @@
+
 from django.db import transaction
 from rest_framework import serializers
-from core.models import User, UserAlias
+from core.models import User, UserAlias, UserGroup
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -24,72 +25,87 @@ class UserCreateSerializer(serializers.ModelSerializer):
         fields = ["id", "name"]
 
     def validate_id(self, value):
-        if UserAlias.objects.filter(id=value).exists():
-            raise serializers.ValidationError(
-                "UWA ID is already associated with a user"
-            )
+        # Ensure UWA ID is 8 digits (validator on model also does this)
+        if not re.match(r"^\d{8}$", value):
+            raise serializers.ValidationError("Invalid UWA ID")
+        if User.objects.filter(id=value).exists():
+            raise serializers.ValidationError("UWA ID already exists")
         return value
 
     @transaction.atomic
     def create(self, validated_data):
         user = User.objects.create(**validated_data)
-        user.aliases.create(id=user.id)
+        # Save initial alias (primary ID as alias)
+        UserAlias.objects.create(user=user, id=user.id)
         return user
 
 
-class UserUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "name", "role"]
+class UserUpdateSerializer(serializers.Serializer):
+    # Update name and role; ID change happens via PATCH with id field (must be an alias)
+    id = serializers.CharField(required=False)
+    name = serializers.CharField(required=False, allow_blank=False)
+    role = serializers.ChoiceField(choices=User.ROLE_CHOICES, required=False)
 
-    def validate_id(self, value):
+    def validate(self, attrs):
         user = self.instance
-        if not user.aliases.filter(id=value).exists():
-            raise serializers.ValidationError(
-                "UWA ID is not one of the user's existing aliases"
-            )
-        return value
+        if "id" in attrs:
+            new_id = attrs["id"]
+            # Must be one of existing aliases
+            if not user.aliases.filter(id=new_id).exists():
+                raise serializers.ValidationError({"id": "ID must be one of the user's aliases"})
+        return attrs
+
+    @transaction.atomic
+    def save(self, **kwargs):
+        user = self.instance
+        if "name" in self.validated_data:
+            user.name = self.validated_data["name"]
+        if "role" in self.validated_data:
+            user.role = self.validated_data["role"]
+        if "id" in self.validated_data:
+            # change primary UWA ID
+            new_id = self.validated_data["id"]
+            # Update primary key: we need to set user.id and save; aliases already includes new_id
+            user.id = new_id
+        user.save()
+        return user
 
 
-class UserAliasCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id"]
+class UserAliasCreateSerializer(serializers.Serializer):
+    id = serializers.CharField()
 
     def validate_id(self, value):
+        # must be 8 digits
+        if not re.match(r"^\d{8}$", value):
+            raise serializers.ValidationError("Invalid UWA ID")
+        if User.objects.filter(id=value).exists():
+            raise serializers.ValidationError("UWA ID already used by another user")
         if UserAlias.objects.filter(id=value).exists():
-            raise serializers.ValidationError(
-                "UWA ID is already associated with a user"
-            )
+            raise serializers.ValidationError("UWA ID already used as an alias")
         return value
 
+    @transaction.atomic
     def save(self):
-        user = self.instance
+        user: User = self.instance
         alias_id = self.validated_data["id"]
-        user.aliases.create(id=alias_id)
+        UserAlias.objects.create(user=user, id=alias_id)
         return user
 
 
-class UserAliasDeleteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id"]
+class UserAliasDeleteSerializer(serializers.Serializer):
+    id = serializers.CharField()
 
     def validate_id(self, value):
-        user = self.instance
+        user: User = self.instance
         if not user.aliases.filter(id=value).exists():
-            raise serializers.ValidationError(
-                "UWA ID is not one of the user's existing aliases"
-            )
+            raise serializers.ValidationError("UWA ID is not one of the user's existing aliases")
         if value == user.id:
-            raise serializers.ValidationError(
-                "Cannot remove the primary UWA ID of a user"
-            )
+            raise serializers.ValidationError("Cannot remove the primary UWA ID of a user")
         return value
 
+    @transaction.atomic
     def save(self):
-        user = self.instance
+        user: User = self.instance
         alias_id = self.validated_data["id"]
-        alias = user.aliases.get(id=alias_id)
-        alias.delete()
+        user.aliases.filter(id=alias_id).delete()
         return user
