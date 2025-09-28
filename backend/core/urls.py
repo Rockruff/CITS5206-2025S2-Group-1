9034d522d9ce73d6bc634e2b3814da194de301dc
Gayathri <gkkanakaratne@gmail.com>
2025-09-28 11:17:47 +0800

    API: register /api/training-records in router

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,10 +3,12 @@ from rest_framework.routers import DefaultRouter
 from .views.users import UserViewSet
 from .views.groups import UserGroupViewSet
 from .views.trainings import TrainingViewSet
+from .views.training_records import TrainingRecordViewSet  # <-- added
 
 router = DefaultRouter(trailing_slash=False)
 router.register("users", UserViewSet, basename="user")
 router.register("groups", UserGroupViewSet, basename="group")
 router.register("trainings", TrainingViewSet, basename="training")
+router.register("training-records", TrainingRecordViewSet, basename="training-record")  # <-- added
 
 urlpatterns = router.urls
