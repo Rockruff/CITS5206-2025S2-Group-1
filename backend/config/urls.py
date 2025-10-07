0186d0247b485b6caeaea15f1000424907d5c779
Rockruff <36600169+Rockruff@users.noreply.github.com>
2025-10-07 16:45:03 +0800

    chore: remove unused items in backend settings

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,17 +1,9 @@
 # config/urls.py
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     # JWT auth
     path("api/auth/", include("auth.urls")),
     # API routers
     path("api/", include("core.urls")),
-    # API schema/docs
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/docs/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
 ]
